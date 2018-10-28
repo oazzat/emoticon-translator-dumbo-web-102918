8741bd1ac9f005339f0732a86ed2b6bc9075f7b3
Omar A <oazzat19@gmail.com>
2018-10-28 18:55:31 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ def load_library (file)
   hash = YAML.load_file(file)
   new_hash = {:get_meaning,:get_emoticon}
   hash.each do |key,val|
-    new_hash[key]
+    new_hash[get_meaning][key]
   end
 end
 
