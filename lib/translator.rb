8e83368e48919a5283f32e36c698b03ba2c367bb
Omar A <oazzat19@gmail.com>
2018-10-28 19:20:43 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ end
 def get_japanese_emoticon (filep,emo)
   hash = load_library(filep)
   
-  if hash.values.include?(emo)
+  if hash.values.flatten.include?(emo)
     return hash[get_emoticon][emo]
   else 
     return "Sorry"
